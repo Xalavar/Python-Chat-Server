ee583df21f10812f0d414b9724173504f519aa2a
Xalavar <nicholasminor@csus.edu>
2023-11-24 14:58:05 -0800

    Added bcst function

--- client.py
+++ client.py
@@ -11,7 +11,7 @@ def receive_messages(sock):
             if not data:
                 break
             sys.stdout.flush()  # Flush the output buffer
-            print("From server: ", data)
+            print(f"{data}") # server response
             sys.stdout.flush()  # Flush the output buffer
         except socket.error as e:
             sys.stdout.flush()  # Flush the output buffer
@@ -23,7 +23,7 @@ def main():
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     # Verify that there are 3 command line arguments (file, hostname, port)
     if len(sys.argv) != 3:
-        print("Usage: python3 pingcli.py <hostname> <port>")
+        print("Usage: python3 client.py <hostname> <port>")
         sys.exit(1)  # terminate program
 
     # Grab details from the command line arguments
@@ -41,15 +41,19 @@ def main():
 
         # ask user to join server with username & store username data
         join_prompt = str(input("Enter JOIN followed by your username: "))
-        username = join_prompt.split(" ")[1]
+        username = join_prompt.split(" ", 1)
         cli_sock.send(join_prompt.encode())
 
         while True:
             try:
                 message = input()
                 # to get client's request type
-                message_array = message.split(" ")
-                request = message_array[0]
+                request = message.split(" ")[0]
+                
+                # checking if the request has an input string (everything after request)
+                message_array = message.split(" ", 1)
+                if len(message_array) > 1:
+                    message_array = message_array[1]
 
                 cli_sock.sendall(message.encode())
 
